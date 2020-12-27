a7c4a2a251dd29e164c97914c16ebff7fa67da64
Marcelo Freitas <jmfda00@gmail.com>
2020-12-27 15:58:45 -0300

    Adiciona o esqueleto inical do trabalho

---
+++ bresenham.js
@@ -0,0 +1,5 @@
+function bresenhamAlgorithm(){
+    // Implementação do algoritmos de desenho de 
+    // linhas de Bresenham
+    return 'Ainda não implementado';
+}
---
+++ sketch.js
@@ -0,0 +1,80 @@
+// Variáveis de escopo global
+var frameBuffer;
+// Quantidade de quadrados por linha e por coluna
+const rows = 25;
+const cols = 25; 
+// Largura e altura do canvas
+const width = 600;
+const height = 600;
+// Tamanho do lado do quadrado
+const squareSide = Math.floor( width/rows );
+
+
+function setup() {
+  // método que faz o preparo do desenho
+  createCanvas(width, height);
+  frameBuffer = makeFrameBuffer(rows, cols);
+}
+
+function draw() {
+  // método que é chamado constantemente para atualizar o desenho
+  background(200);
+  drawFrameBuffer();
+}
+
+
+/************ Métodos de desenho auxiliares **********************/
+
+
+function makeFrameBuffer(rows, cols) {
+  // Método que cria a matriz de pontos que armazena 
+  // os pixels a desenhar  na tela (frame buffer)
+  // deve ser chamado ao preparar o canvas (método setUp)
+
+  var arr = new Array(rows);
+  for (var i = 0; i < rows; i++)
+    arr[i] = new Array(cols).fill(0);
+  return arr;
+}
+
+
+function drawFrameBuffer() {
+  // usa as informações sobre pixels a desenhar
+  // contidas no frameBufer e desenha elas em uma grade
+  // de quadrados
+  // é chamado repetidamente pelo método draw
+  
+  for (var x = 0; x < rows; x++) {
+    for (var y = 0; y < cols; y++) {
+
+      var px = x * squareSide;
+      var py = y * squareSide;
+
+      stroke(0);
+      strokeWeight(0.7);
+
+      if (frameBuffer[x][y] == 1) fill(0);
+      else noFill();
+      square(px, py, squareSide);
+    }
+  }
+}
+
+function mouseClicked(){
+  // Executa o código abaixo quando o mouse é clicado
+  markSquare();
+}
+
+function markSquare() {
+  // método auxiliar para marcar um quadrado
+  // baseado nas coordenadas x e y do mouse quando clicado
+  // o quadrado marcado será armazenado no frame buffer
+  
+  // descarta cliques fora da área do canvas
+  if (mouseX > width || mouseY > height) return; 
+  let rowClicked = Math.floor(mouseX/squareSide);
+  let colClicked = Math.floor(mouseY/squareSide);
+
+  // Atualiza o frame buffer
+  frameBuffer[rowClicked][colClicked] = 1;
+}
